aedd33f3020d5bada77fdb33cb167e04583f9244
Allan Silva <allandasilva33@outlook.com>
2025-01-07 23:11:06 -0300

    Estruturação e estilização do componente Card completa

--- src/components/Home-components/Card.jsx
+++ src/components/Home-components/Card.jsx
@@ -1,8 +1,23 @@
-function Card() {
+import TrashIcon from '../../assets/icons/trash-2.svg'
+
+function Card(props) {
 
     return (
       <>
-        
+      {props.propUsers.map((ele)=>(
+        <div key={ele.id} className='flex items-center gap-4 w-full p-4 rounded-lg shadow-md bg-indigo-900'>
+          <div className='flex flex-col gap-1 w-full font-semibold text-white'>
+            <p>Nome: {ele.name}<span className='font-normal'></span></p>
+            <p>Username: {ele.username}<span className='font-normal'></span></p>
+            <p>E-mail: {ele.email}<span className='font-normal'></span></p>
+          </div>
+          <div>
+            <button className='flex items-center'>
+              <img src={TrashIcon} alt="Trash" />
+            </button>
+          </div>
+        </div>
+      ))}
       </>
     )
   }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,33 @@
 import Form from "../components/Home-components/Form"
 import Card from "../components/Home-components/Card"
+import { useState } from "react"
 
 function Home() {
-
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      name: 'Allan',
+      username: 'Griez',
+      email: 'allan@email',
+    },
+    {
+      id: 2,
+      name: 'Messi',
+      username: 'GOAT',
+      email: 'messi@email',
+    },
+    {
+      id: 3,
+      name: 'Veiga',
+      username: 'Principe',
+      email: 'veiga@email',
+    },
+  ])
   return (
     <>
-      <main className="flex flex-col gap-6 w-full max-w-lg mx-auto p-6">
+      <main className="flex flex-col justify-center gap-6 min-h-screen w-full max-w-lg mx-auto p-6">
         <Form />
-        <Card />
+        <Card propUsers={users} />
       </main>
     </>
   )
