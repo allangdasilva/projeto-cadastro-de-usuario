6c6a8006d380c5799f0278bd0207cfb504fd9e43
Allan Silva <allandasilva33@outlook.com>
2025-01-07 22:13:38 -0300

    Configurações base do projeto | Bibliotecas e pastas

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Form from "../components/Home-components/Form"
+import Card from "../components/Home-components/Card"
+
+function Home() {
+
+  return (
+    <>
+      <main>
+        <Form />
+        <Card />
+      </main>
+    </>
+  )
+}
+
+export default Home
